18d9851255c26cb865f8b52d66315dcee4a97cf0
ericadubie <ericadubie14@gmail.com>
2024-03-13 14:44:13 -0400

    started statistics/index page styling

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,10 +1,56 @@
 <script setup lang="ts">
+    const props = defineProps<{
+    distance: number;
+    duration: number;
+    avgPace: number;
+    calories: number;
+}>();
 
 </script>
 
 <template>
-    <div>
-        <h1>hey</h1>
+    <div class="container">
+        <div class="columns is-centered">
+            <div class="column is-half media">
+                <div class="box summary" style="margin: 5px;">
+                    <h2 class="title" style="text-align: center;"> Today </h2>
+                    <div class="columns">
+                        <div class="column is-multiline">
+                            <div class="column is-half">
+                                <h3 class="value">{{ distance }}</h3>
+                                <caption class="caption">
+                                Distance
+                                </caption>
+                            </div>
+                            <div class="column is-half">
+                                <h3 class="value">{{ duration }}</h3>
+                                <caption class="caption">
+                                Duration
+                                </caption>
+                            </div>
+                            <div class="column is-half">
+                                <h3 class="value">{{ avgPace }}</h3>
+                                <caption class="caption">
+                                Avg Pace
+                                </caption>
+                            </div>
+                            <div class="column is-half">
+                                <h3 class="value">{{ calories }}</h3>
+                                <caption class="caption">
+                                Calories
+                                </caption>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="box" style="margin: 5px;">
+                    hey
+                </div>
+                <div class="box" style="margin: 5px;">
+                    hey
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
